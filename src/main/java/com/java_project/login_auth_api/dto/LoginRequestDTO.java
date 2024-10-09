40dcf6c5b139a23c2690efbb3a73897a2992be2a
Hendrick Menacho <hmenacho97@gmail.com>
2024-10-09 18:31:57 -0300

    first commit

---
+++ src/main/java/com/java_project/login_auth_api/dto/LoginRequestDTO.java
@@ -0,0 +1,3 @@
+package com.java_project.login_auth_api.dto;
+
+public record LoginRequestDTO (String email, String password) {}
---
+++ src/main/java/com/java_project/login_auth_api/dto/ResponseDTO.java
@@ -0,0 +1,4 @@
+package com.java_project.login_auth_api.dto;
+
+public record ResponseDTO(String name, String token) {
+}
